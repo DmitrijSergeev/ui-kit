2b65e254fcfc8301299894d7d3d278302dfceb3d
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-07-05 22:11:51 +0400

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+
+import { devDependencies, peerDependencies } from './package.json'
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(peerDependencies),
+        ...Object.keys(devDependencies),
+      ],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
 })
