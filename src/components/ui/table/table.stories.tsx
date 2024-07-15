7baf36b84a578aea63c52f3f20cb9de7e42e4f2f
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-07-15 18:11:32 +0400

    table

---
+++ src/components/ui/table/table.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Table } from './table'
+
+const meta = {
+  component: Table,
+  tags: ['autodocs'],
+  title: 'Components/Table',
+} satisfies Meta<typeof Table>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    children: 'Default button',
+    disabled: false,
+  },
+}
---
+++ src/components/ui/table/table.tsx
@@ -0,0 +1,4 @@
+import s from './table.module.scss'
+export const Table = () => {
+  return <div></div>
+}
