c1e540e13ca37f32bb54444e2c7255dc13fc9915
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-07-16 17:31:51 +0400

    select

---
+++ src/components/ui/select/select.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Select } from './select'
+
+const meta = {
+  component: Select,
+  tags: ['autodocs'],
+  title: 'Components/Select',
+} satisfies Meta<typeof Select>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
