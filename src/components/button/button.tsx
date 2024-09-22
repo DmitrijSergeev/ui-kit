c922e9b70eaa69a3b7f91d76e2df8da125634dcd
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-09-22 14:52:44 +0400

    app

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,9 +1,10 @@
 import {ComponentPropsWithoutRef} from "react";
+import s from './button.module.css'
 
 type Props = ComponentPropsWithoutRef<'button'>
 export const Button = (props: Props) => {
     return (
-        <button {...props}>
+        <button {...props} className={s.button}>
             Hello World!!!
         </button>
     );
