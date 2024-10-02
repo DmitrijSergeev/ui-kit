7c5a4309cecfd7b7313f0c645c57a9d841df8961
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-10-02 20:08:56 +0400

    textarea component

--- src/ui/textField/textFieldArea/textFieldArea.tsx
+++ src/ui/textField/textFieldArea/textFieldArea.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import s from './textFieldArea.scss'
+import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
+import s from './textFieldArea.module.scss'
+import clsx from 'clsx'
 
-export const TextFieldArea = () => {
-  return (
-    <div>
-      <label></label>
-      <textarea></textarea>
-    </div>
-  )
-}
+export type TextAreaProps = {
+  classNameTextArea?: string
+  error?: string
+  label?: string
+  resize?: 'both' | 'horizontal' | 'none' | 'vertical'
+} & ComponentPropsWithoutRef<'textarea'>
+
+export const TextFieldArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (
+    { label, classNameTextArea, resize = 'both', className, error, id = label, disabled, ...props },
+    ref
+  ) => {
+    const classNames = {
+      container: clsx(s.container, className),
+      label: clsx(s.label, disabled && s.disabled),
+      textarea: clsx(error && s.error, s.textarea, classNameTextArea),
+    }
+    return (
+      <div className={classNames.container}>
+        {label && (
+          <label className={classNames.label} htmlFor={id}>
+            {label}
+          </label>
+        )}
+        <textarea
+          className={classNames.textarea}
+          disabled={disabled}
+          id={id}
+          ref={ref}
+          style={{ resize: resize, ...props.style }}
+          {...props}
+        ></textarea>
+        {error && <span className={s.error}>{error}</span>}
+      </div>
+    )
+  }
+)
